70e5ec7752e9c31ba667fbeebac30c50c5355e88
JodiBonJodi <joedvice@gmail.com>
2019-09-30 17:15:36 -0700

    This is my first commit for this repo since class started. This contains all the work from lecture and the beginning of my lab

---
+++ app.js
@@ -0,0 +1,24 @@
+import { isCorrect } from './functions.js';
+
+const myButton = document.getElementById('action-button');
+
+
+myButton.onclick = () => {
+    alert('welcome to my quiz');
+    const myConfirmation = confirm('do you want to take my quiz');
+
+    console.log(myConfirmation);
+
+    const userAnswer = prompt('is my favorite color blueish? (yes/no)');
+    const sanitizedUserAnswer = userAnswer.toLowerCase().trim();
+
+    console.log(userAnswer);
+
+    if (userIsCorrect) {
+        alert('Right-o!');
+    } else {
+        alert("Nah");
+    }
+
+};
+
