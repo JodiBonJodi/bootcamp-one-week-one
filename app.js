497adda7c57d4985d943ad9cdadd18f62853400c
JodiBonJodi <joedvice@gmail.com>
2019-10-04 11:46:05 -0700

    played with css a bit, made the pics have rounded edges

--- app.js
+++ app.js
@@ -11,29 +11,24 @@ myButton.addEventListener('click', () => {
     
     const myConfirmation = prompt(userName + ', Do you want to take my quiz');
 
-    console.log(myConfirmation);
-
     const userSaysYes = isYes(myConfirmation);
 
     if (userSaysYes) {
         const userAnswer1 = prompt('Hey ' + userName + ', did JBJ study snakes in college?');
         const sanitizedUserAnswer1 = userAnswer1.toLowerCase().trim();
 
-        console.log(sanitizedUserAnswer1);
         const userIsCorrect1 = isYes(sanitizedUserAnswer1);
 
 
-        const userAnswer2 = prompt('So ' + userName +', does JBJ play roller derby?');
-        const sanitizedUserAnswer2= userAnswer2.toLowerCase().trim();
+        const userAnswer2 = prompt('So ' + userName + ', does JBJ play roller derby?');
+        const sanitizedUserAnswer2 = userAnswer2.toLowerCase().trim();
 
-        console.log(sanitizedUserAnswer2);
         const userIsCorrect2 = isYes(sanitizedUserAnswer2);
 
 
         const userAnswer3 = prompt('Ok ' + userName + ", is JBJ's dog's name Rosey?");
         const sanitizedUserAnswer3 = userAnswer3.toLowerCase().trim();
 
-        console.log(sanitizedUserAnswer3);
         const userIsCorrect3 = isYes(sanitizedUserAnswer3);
 
         alert('You are now done with this quiz. Youre results will be available on the About Me page.');
@@ -65,9 +60,6 @@ myButton.addEventListener('click', () => {
         user.textContent = userName;
         score.textContent = scoreCount;
         percent.textContent = percentScore;
-
-
-       // (userName + ', you did great! You got a ' + scoreCount + '/3 on this quiz!');
 
     } else {
         alert('Cool, I guess you dont want to take my quiz.');
