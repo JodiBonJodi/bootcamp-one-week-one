128e2f12b4acbc0c8f6544f24f0c5ca4b7f69e73
JodiBonJodi <joedvice@gmail.com>
2019-10-04 13:22:26 -0700

    fixed a bug to pass the travis

--- practiceapp.js
+++ practiceapp.js
@@ -5,21 +5,19 @@ const myButton = document.getElementById('action-button');
 
 myButton.onclick = () => {
     alert('welcome to my quiz');
-    const myConfirmation = confirm('do you want to take my quiz');
+    confirm('do you want to take my quiz');
 
-    console.log(myConfirmation);
 
     const userAnswer = prompt('is my favorite color blueish? (yes/no)');
     const sanitizedUserAnswer = userAnswer.toLowerCase().trim();
 
-    console.log(userAnswer);
 
     const userIsCorrect = isCorrect(sanitizedUserAnswer);
 
     if (userIsCorrect) {
         alert('Right-o!');
     } else {
-        alert("Nah");
+        alert('Nah');
     }
 
 };
